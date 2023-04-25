d1bdc28c2f242eadf66577187f29ad8727c260f3
wudi <devon.wu@aliyun.com>
2023-04-25 14:32:26 +0800

    Exception  Optimize

--- src/main/java/com/threesides/exception/BusinessException.java
+++ src/main/java/com/threesides/exception/BusinessException.java
@@ -21,21 +21,52 @@ public class BusinessException extends BaseException{
 		super(errMessage, ResponseResultCodeEnum.BUSINESS_ERROR.getKey());
 	}
 
+	/**
+	 * 业务异常
+	 *
+	 * @param errMessage 异常信息
+	 * @param errCode 异常状态
+	 */
 	public BusinessException(String errMessage, Object errCode) {
 		super(errMessage, errCode);
 	}
 
+	/**
+	 * 业务异常
+	 *
+	 * @param errMessage 异常信息
+	 * @param cause 原因
+	 */
 	public BusinessException(String errMessage, Throwable cause) {
 		super(errMessage, cause,ResponseResultCodeEnum.BUSINESS_ERROR.getKey());
 	}
 
+	/**
+	 * 业务异常
+	 *
+	 * @param errMessage 异常信息
+	 * @param cause 原因
+	 * @param errCode 异常状态
+	 */
 	public BusinessException(String errMessage, Throwable cause, Object errCode) {
 		super(errMessage, cause, errCode);
 	}
 
+	/**
+	 * 业务异常
+	 *
+	 * @param beanEnum 基础枚举
+	 */
 	public BusinessException(BeanEnum beanEnum) {
 		super(beanEnum.getValue(),  beanEnum.getKey());
 	}
+
+	/**
+	 * 业务异常
+	 *
+	 * @param cause 原因
+	 * @param beanEnum 基础枚举
+	 */
 	public BusinessException(Throwable cause,BeanEnum beanEnum) {
 		super(beanEnum.getValue(), cause, beanEnum.getKey());
 	}
--- src/main/java/com/threesides/exception/SystemException.java
+++ src/main/java/com/threesides/exception/SystemException.java
@@ -5,32 +5,68 @@ import com.threesides.constant.enums.ResponseResultCodeEnum;
 import com.threesides.lang.ObjectUtil;
 
 /**
- * ParameterException
+ * SystemException
  *
  * @author Di Wu
  * @since 2023-04-09
  */
 public class SystemException extends BaseException{
 
+	/**
+	 * 系统异常
+	 *
+	 * @param errMessage 异常信息
+	 */
 	public SystemException(Object errMessage) {
 		super(errMessage, ResponseResultCodeEnum.SYSTEM_ERROR.getKey());
 	}
 
+	/**
+	 * 系统异常
+	 *
+	 * @param errMessage 异常信息
+	 * @param errCode 异常状态
+	 */
 	public SystemException(Object errMessage, Object errCode) {
 		super(errMessage, errCode);
 	}
 
+	/**
+	 * 系统异常
+	 *
+	 * @param errMessage 异常信息
+	 * @param cause 原因
+	 */
 	public SystemException(Object errMessage, Throwable cause) {
 		super(errMessage, cause, ResponseResultCodeEnum.SYSTEM_ERROR.getKey());
 	}
 
+	/**
+	 * 系统异常
+	 *
+	 * @param errMessage 异常信息
+	 * @param cause 原因
+	 * @param errCode 异常状态
+	 */
 	public SystemException(Object errMessage, Throwable cause, Object errCode) {
 		super(errMessage, cause, errCode);
 	}
 
+	/**
+	 * 系统异常
+	 *
+	 * @param beanEnum 基础枚举
+	 */
 	public SystemException(BeanEnum beanEnum) {
 		super(beanEnum.getValue(),  beanEnum.getKey());
 	}
+
+	/**
+	 * 系统异常
+	 *
+	 * @param cause 原因
+	 * @param beanEnum 基础枚举
+	 */
 	public SystemException(Throwable cause,BeanEnum beanEnum) {
 		super(beanEnum.getValue(), cause, beanEnum.getKey());
 	}
